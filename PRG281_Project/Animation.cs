1108c37774a3eea363951224c0322a4f43a7c0a4
Drag0nSlay3rZ1 <rivan.maritz@gmail.com>
2025-08-19 15:40:52 +0200

    Interfaces added to the solution

--- PRG281_Project/Animation.cs
+++ PRG281_Project/Animation.cs
@@ -6,17 +6,25 @@ using System.Threading.Tasks;
 
 namespace PRG281_Project
 {
-    public class Animation
+
+    interface IAnimation
+    {
+        void DisplayCenteredText(string text, int row);
+        void AnimateCenteredTyping(string text, int row, int promptLength);
+        void LoadingBar();
+    }
+
+    public class Animation : IAnimation
     {
         //method to display text in the center of the console window
-        public static void DisplayCenteredText(string text, int row)
+        public void DisplayCenteredText(string text, int row)
         {
             int col = (Console.WindowWidth - text.Length) / 2;
             Console.SetCursorPosition(col, row);
             Console.Write(text);
         }
         //method to animate typing effect for text in the center of the console window
-        public static void AnimateCenteredTyping(string text, int row, int promptLength)
+        public void AnimateCenteredTyping(string text, int row, int promptLength)
         {
             int startCol = (Console.WindowWidth - (promptLength + 1 + text.Length)) / 2 + promptLength + 7;
             Console.SetCursorPosition(startCol, row);
@@ -28,7 +36,7 @@ namespace PRG281_Project
             }
         }
 
-        public static void LoadingBar()
+        public void LoadingBar()
         {
             Console.Clear();
             int totalWidth = 50;
--- PRG281_Project/LoginDisplay.cs
+++ PRG281_Project/LoginDisplay.cs
@@ -5,11 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PRG281_Project
-{
+{ 
+
     class LoginDisplay
     {
         public void DisplayOpening()
         {
+            Animation animation = new Animation();
+
             //text for the password and username animation in the center of the console window
             string usernamePrompt = "Enter username:";
             string passwordPrompt = "Enter password:";
@@ -20,13 +23,13 @@ namespace PRG281_Project
             int startRow = (Console.WindowHeight / 2) - (totalLines / 2);
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Animation.DisplayCenteredText(usernamePrompt, startRow);
-            Animation.DisplayCenteredText(passwordPrompt, startRow + 1);
+            animation.DisplayCenteredText(usernamePrompt, startRow);
+            animation.DisplayCenteredText(passwordPrompt, startRow + 1);
             Console.ResetColor();
 
-            Animation.AnimateCenteredTyping(username, startRow, usernamePrompt.Length);
+            animation.AnimateCenteredTyping(username, startRow, usernamePrompt.Length);
 
-            Animation.AnimateCenteredTyping(password, startRow + 1, passwordPrompt.Length);
+            animation.AnimateCenteredTyping(password, startRow + 1, passwordPrompt.Length);
 
             Console.Clear();
 
@@ -34,7 +37,7 @@ namespace PRG281_Project
             security.GetInformation(); // Call the method to get user input for login validation
 
 
-            Thread loadingThread = new Thread(Animation.LoadingBar);                //CUSTOM THREADING
+            Thread loadingThread = new Thread(animation.LoadingBar);                //CUSTOM THREADING
             loadingThread.Start();      // Starting the thread loadingThread
             loadingThread.Join();       // Wait for the loading bar to finish
 
